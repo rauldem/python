e7e1b8bafa8f7edaa436c051c42879dd6769ebe9
rauldem <rauldmingo@gmail.com>
2017-02-13 11:26:49 +0100

    Add files via upload

---
+++ rsync.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+'''
+# Script Name        : rsync.py
+# Author             : Raul de Mingo
+# Created            : 10th February 2017
+# Last Modified      :
+# Version            : 1.0
+# Modifications      :
+
+# Description        : Directory Synchronization
+'''
+
+import os
+import shlex, subprocess
+import time
+
+#fecha    = datetime.date.today()        # Get Today's date
+fecha = time.strftime('%Y%m%d%H%M%S')   #in the format YYYYMMDDHHMMSS
+  
+def configurapath(ruta):
+    tamano = len(ruta)
+    if ruta[tamano - 1] != "/":
+        ruta = ruta + "/"
+    return ruta
+
+def chequearuta(ruta):
+    if not os.path.exists(ruta):         # if not os.path.isdir(ruta)
+        print("La ruta indicada " + ruta + " no existe")
+        #os.mkdir(ruta)
+        return "False"
+    else:
+        return "True"
+
+print("Sincronizacion de directorios en Linux mediante RSYNC")
+origen = input("Indica el path de origen:  /path/directorio/ ")
+# Chequeamos que el path finalice con "/"
+origen = configurapath(origen)
+# Chequear que el path existe.
+if chequearuta(origen) == "False":
+    exit(1)        # break
+
+destino = input("Indica el path de destino:  /path/directorio/ ")
+destino = configurapath(destino)
+if chequearuta(destino) == "False":
+    exit(1)        # break
+
+comando1 = 'rsync --av --delete ' + origen +' ' + destino + ' >> /tmp/rysnc.' + fecha + '.log'
+comando2 = shlex.split(comando1)
+print("Ejecutando comando " + comando2)
+subprocess.call(comando2)
---
+++ users-name-check.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+'''
+# Script Name        : users-name-check.py
+# Author             : Raul de Mingo
+# Created            : 9th February 2017
+# Last Modified      :
+# Version            : 1.0
+# Modifications      :
+
+# Description        : Check rules for create new users 
+'''
+
+condicion = 0
+
+while condicion == 0:
+
+    # User name request
+    print("Por favor indicanos el nombre de usuario que quieres crear")
+    nombre = input("Nombre usuario: ")
+    
+    condicion = 1
+    
+    # Between 6 and 12 characters
+    if len(nombre) < 6:
+        print("Debe especificar un tamano mayor de seis caracteres")
+        condicion = 0
+    elif len(nombre) > 12:
+        print("Debe especificar un tamano menor de doce caracteres")
+        condicion = 0
+    # Name must be alphanumeric
+    elif nombre.isalnum() == False:
+        print("El nombre de usuario debe ser alfanumerico")
+        condicion = 0
+    else:
+        print("El nombre de usuario " + nombre + " cumple todas las normativas")
